274a7eac5cbfa73b6422958504bfdf0d03892633
Rizky Benggolo <travarilo@gmail.com>
2022-07-08 18:17:16 +0700

    extras: Cleanup vendor_extras
    Change-Id: I2c373d0894548560078daf2711d61aab39928916

---
+++ extras.mk
@@ -0,0 +1,17 @@
+# Audio
+$(call inherit-product, vendor/extras/audio/audio.mk)
+
+# Bootanimation
+TARGET_SCREEN_WIDTH ?= 1080
+TARGET_SCREEN_HEIGHT ?= 1920
+PRODUCT_PACKAGES += \
+    bootanimation.zip
+
+# Custom Icons
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/extras/customicons
+
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
